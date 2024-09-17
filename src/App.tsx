1c8292d215ee0683e00db9eea8cb08a6457c254e
farhod <farhod.jonikulovich@gmail.com>
2024-09-17 17:10:26 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import "./styles/index.scss";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
+import { useTheme } from "./styles/theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <div
+      className={classNames("app", { hovered: true, selected: true }, [
+        theme,
+        "cls",
+      ])}
+    >
+      <button onClick={toggleTheme}>switch theme</button>
+      <Link to={"/"}>main</Link>
+      <Link to={"/about"}>about</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
